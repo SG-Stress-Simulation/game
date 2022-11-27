8edec55dbf2a609b96a1c9f9c32fb67ee94c83be
Felix Nagel <behalten_steil_09@icloud.com>
2022-11-27 01:18:17 +0100

    :sparkles:: Add outline to scarecrow hat

---
+++ Assets/Scripts/OutlineBehaviour.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineBehaviour : MonoBehaviour
+{
+    public GameObject sourceLeft;
+    public GameObject sourceRight;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // Fetch collider and check if it exists
+        Debug.Log("Trigger for " + gameObject.name + " is On: " + GetComponent<Collider>().isTrigger);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other == sourceLeft.GetComponent<Collider>() || other == sourceRight.GetComponent<Collider>())
+        {
+            Debug.Log("Trigger entered");
+            var outline = gameObject.AddComponent<Outline>();
+            outline.OutlineMode = Outline.Mode.OutlineAll;
+            outline.OutlineColor = Color.red;
+            outline.OutlineWidth = 5f;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(GetComponent<Outline>());
+    }
+}
