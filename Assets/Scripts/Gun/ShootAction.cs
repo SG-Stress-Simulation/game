4423f71f1a8d52479ba15270e862c30f697799aa
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2022-12-12 15:06:22 +0100

    started implementation of handgun mechanic

---
+++ Assets/Scripts/Gun/ShootAction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAction : MonoBehaviour
+{
+    // Update is called once per frame
+    public void Shoot()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(gameObject. transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.tag == "Enemy")
+            {
+                Debug.Log("Enemy hit");
+            }
+        }
+    }
+}
