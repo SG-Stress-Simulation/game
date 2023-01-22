4fe88f23943a8f117925324ff6ff04e207a9d6ab
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-01-22 23:48:18 +0100

    multiple effects and new color effect

--- Assets/Scripts/Levels/Level1.cs
+++ Assets/Scripts/Levels/Level1.cs
@@ -5,6 +5,12 @@ using UnityEngine.Rendering.PostProcessing;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+enum EffectType
+{
+  FOVDecrease,
+  ColorLoss,
+}
+
 public class Level1 : MonoBehaviour
 {
   public float animationDistance = 3f;
@@ -12,9 +18,11 @@ public class Level1 : MonoBehaviour
   float timeToNextEffect = 2f;
   float timeToEffectEnd = Mathf.PI * 5;
   bool effectStarted = false;
-
+  private EffectType currentEffect = EffectType.FOVDecrease;
+  
    PostProcessVolume m_Volume;
    Vignette m_Vignette;
+   ColorGrading m_ColorGrading;
   
   private IEnumerator AnimateLevelStart()
   {
@@ -37,12 +45,28 @@ public class Level1 : MonoBehaviour
   
   void UpdateEffect()
   {
-    m_Vignette.intensity.Override( Mathf.Sin(timeToEffectEnd / 5) * 0.5f + (Mathf.Sin(Time.realtimeSinceStartup)) * 0.2f);
+    switch (currentEffect)
+    {
+      case EffectType.FOVDecrease:
+        m_Vignette.intensity.Override( Mathf.Sin(timeToEffectEnd / 5) * 0.5f + (Mathf.Sin(Time.realtimeSinceStartup)) * 0.2f);
+        break;
+      case EffectType.ColorLoss:
+        m_ColorGrading.saturation.Override(Mathf.Sin(timeToEffectEnd / 5));
+        break;
+    }
   }
   
   void DisableEffect()
   {
-    m_Vignette.intensity.Override( 0f);
+    switch (currentEffect)
+    {
+      case EffectType.FOVDecrease:
+        m_Vignette.intensity.Override( 0f);
+        break;
+      case EffectType.ColorLoss:
+        m_ColorGrading.saturation.Override( 0f);
+        break;
+    }
     timeToNextEffect = Random.Range(2f, 2f);
     timeToEffectEnd = Mathf.PI * 5;
   }
@@ -60,8 +84,13 @@ public class Level1 : MonoBehaviour
       // Create an instance of a vignette
        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
        m_Vignette.enabled.Override(true);
-      // Use the QuickVolume method to create a volume with a priority of 100, and assign the vignette to this volume
-       m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+       
+// Create an instance of a color grading
+        m_ColorGrading = ScriptableObject.CreateInstance<ColorGrading>();
+        m_ColorGrading.enabled.Override(true);
+        
+        // Create a volume and add the post processing effect to it
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette, m_ColorGrading);
   }
 
   // Update is called once per frame
