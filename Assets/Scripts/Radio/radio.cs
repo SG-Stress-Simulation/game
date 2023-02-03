de4824cce2b0acab94d384f3f26407ac61823b2c
Markus-Markewitz <markus.markewitz2@t-online.de>
2023-02-03 19:21:40 +0100

    :bug:: fixed interactable radio

--- Assets/Scripts/Radio/radio.cs
+++ Assets/Scripts/Radio/radio.cs
@@ -5,16 +5,23 @@ using Zinnia.Action;
 
 public class radio : MonoBehaviour
 {
-    public BooleanAction triggerPressed;
+    public BooleanAction leftTriggerPressed;
+    public BooleanAction rightTriggerPressed;
+
+    private bool state;
 
     public AudioClip radioNoise;
     private AudioSource audio;
 
-    public GameObject hand;
+    public GameObject leftHand;
+    public GameObject rightHand;
 
     public Collider collider;
-    private bool state;
 
+    public float debounceTime = 0.5f;
+    private float debounceTimer = 0f;
+
+    public GameObject button;
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +33,44 @@ public class radio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 handPos = hand.transform.position;
 
-        if (Input.GetKeyDown(KeyCode.L) && collider.bounds.Contains(handPos))
+        if (debounceTimer > 0f)
+        {
+            debounceTimer -= Time.deltaTime;
+            return;
+        }
+
+
+        bool leftTrigger = leftTriggerPressed != null ? leftTriggerPressed.Value : false;
+        bool rightTrigger = rightTriggerPressed != null ? rightTriggerPressed.Value : false;
+
+        bool isPressedL = leftTrigger && collider.bounds.Contains(leftHand.transform.position);
+        bool isPressedR = rightTrigger && collider.bounds.Contains(rightHand.transform.position);
+        bool isPressedSim = Input.GetKeyDown(KeyCode.L) && (collider.bounds.Contains(leftHand.transform.position) || collider.bounds.Contains(rightHand.transform.position));
+
+        if (isPressedL || isPressedR || isPressedSim)
         {
             if(state)
             {
                 audio.Stop();
                 state= false;
+
+                button.transform.eulerAngles = new Vector3(
+                    30,
+                    button.transform.eulerAngles.y,
+                    button.transform.eulerAngles.z
+                );
             } else
             {
                 audio.clip = radioNoise;
                 audio.Play();
                 state = true;
+
+                button.transform.eulerAngles = new Vector3(
+                    -30,
+                    button.transform.eulerAngles.y,
+                    button.transform.eulerAngles.z
+                );
             }
 
         }
