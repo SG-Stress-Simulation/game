69de81324a64f32db60258f4cc19baaf307017a7
Ying8080 <57267606+Ying8080@users.noreply.github.com>
2023-02-13 19:00:25 +0800

    add stairs

---
+++ Assets/AssetStoreOriginals/School/Scripts/LightmappedLOD.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class LightmappedLOD : MonoBehaviour
+{
+
+    private MeshRenderer currentRenderer;
+
+    private void Awake()
+    {
+        currentRenderer = gameObject.GetComponent<MeshRenderer>();
+        RendererInfoTransfer();
+    }
+
+#if UNITY_EDITOR
+    void OnBecameVisible()
+    {
+        if (!Application.isPlaying)
+            RendererInfoTransfer();
+    }
+#endif
+
+    void RendererInfoTransfer()
+    {
+        if (GetComponentInParent<LODGroup>() == null)
+            return;
+        var lods = GetComponentInParent<LODGroup>().GetLODs();
+        int currentRendererLodIndex = -1;
+
+        if (currentRenderer == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < lods.Length; i++)
+        {
+            for (int j = 0; j < lods[i].renderers.Length; j++)
+            {
+                if (currentRenderer == lods[i].renderers[j])
+                    currentRendererLodIndex = i;
+            }
+        }
+        if (currentRendererLodIndex == -1)
+        {
+            Debug.Log("Lightmapped LOD : lod index not found on " + gameObject.name);
+            return;
+        }
+
+        var renderers = lods[currentRendererLodIndex].renderers;
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            if (renderers[i] != null)
+            {
+                try
+                {
+                    renderers[i].lightProbeUsage = lods[0].renderers[i].lightProbeUsage;
+                    renderers[i].lightmapIndex = lods[0].renderers[i].lightmapIndex;
+                    renderers[i].lightmapScaleOffset = lods[0].renderers[i].lightmapScaleOffset;
+
+                }
+                catch
+                {
+                    if (Debug.isDebugBuild)
+                        Debug.Log("Lightmapped LOD : Error setting lightmap settings on " + gameObject.name);
+                }
+            }
+        }
+    }
+}
