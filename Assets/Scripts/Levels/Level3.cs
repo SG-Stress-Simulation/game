689b5350f9f9b97a36c271bd7fd03f89b0942bf4
Julian Imhof <julianimhof00@gmail.com>
2023-03-01 18:39:59 +0100

    :sparkles:: add room enter logic to level 3

--- Assets/Scripts/Levels/Level3.cs
+++ Assets/Scripts/Levels/Level3.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Level3 : MonoBehaviour
 {
+    [Header("Level Start")]
+    [Tooltip("The event to trigger when the level moved to the center of the play area")]
+    public UnityEvent onRoomEnter = new UnityEvent();
+    public UnityEvent onRoomEntered = new UnityEvent();
+    public GuidReference collisionForcer;
     public Animator scene;
     public GuidReference menu;
 
     public void OnEnable()
     {
         Debug.Log("Level 3 Started");
-        scene.SetTrigger("Upwards");
     }
 
     public void EndLevel()
@@ -17,6 +22,26 @@ public class Level3 : MonoBehaviour
         Debug.Log("Level 3 Ended");
         scene.SetTrigger("Downwards");
         Invoke("levelEndPostAnimationEvent", 5f);
+    }
+
+    public void StartingSequenceComplete()
+    {
+        collisionForcer.gameObject.SetActive(false);
+        onRoomEntered.Invoke();
+    }
+
+    public void MoveScene()
+    {
+        scene.Play("SidewaysAnim", 0);
+    }
+
+    public void EnterRoom()
+    {
+        onRoomEnter.Invoke();
+        collisionForcer.gameObject.SetActive(true);
+
+        Invoke("MoveScene", 0.2f);
+        Invoke("StartingSequenceComplete", 1f);
     }
 
     public void levelEndPostAnimationEvent()
