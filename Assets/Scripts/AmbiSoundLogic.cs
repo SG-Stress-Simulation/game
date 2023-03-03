d46269ce0a089dba5564fc4d41c425e9c94ec41f
Markus-Markewitz <markus.markewitz2@t-online.de>
2023-03-03 16:41:37 +0100

    :sparkles:: added ambisound for 3rd scene.

---
+++ Assets/Scripts/AmbiSoundLogic.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AmbiSoundLogic : MonoBehaviour
+{
+
+    public GuidReference player;
+    public GameObject target;
+    public UnityEvent onSuccess = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player in target hitbox
+        if (player != null && target != null)
+        {
+            if (target.GetComponent<Collider>().bounds.Contains(player.gameObject.transform.position))
+            {
+                // trigger success event
+                onSuccess.Invoke();
+            }
+        }
+    }
+}
+
