0324f777aeb1541e7f785d5362466614d7304ee1
Julian Imhof <julianimhof00@gmail.com>
2023-03-05 14:28:06 +0100

    :bug:(grab): fix bug where level end could break grabbing

--- Assets/Scripts/Levels/Tutorial.cs
+++ Assets/Scripts/Levels/Tutorial.cs
@@ -1,5 +1,5 @@
+using Tilia.Interactions.Interactables.Interactors;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Tutorial : MonoBehaviour
@@ -9,14 +9,21 @@ public class Tutorial : MonoBehaviour
 
   public GameObject[] steps;
 
+  public IndicatorLight[] indicators;
+
   private int currentStep = 0;
+  
+  public GuidReference leftInteractor;
+  public GuidReference rightInteractor;
 
   public void Start()
   {
     for (int i = 1; i < steps.Length; i++) {
       steps[i].SetActive(false);
+      indicators[i].TurnOff();
     }
     steps[0].SetActive(true);
+    indicators[0].TurnOn();
   }
 
   public void OnEnable()
@@ -35,13 +42,34 @@ public class Tutorial : MonoBehaviour
   public void Step() { 
     int nextStep = (currentStep + 1) % steps.Length;
     steps[currentStep].SetActive(false);
+    indicators[currentStep].TurnOff();
     steps[nextStep].SetActive(true);
+    indicators[nextStep].TurnOn();
     currentStep = nextStep; 
   }
 
   public void levelEndPostAnimationEvent()
   {
     if (menu != null) {
+      
+      InteractorFacade leftFacade = leftInteractor.gameObject.GetComponent<InteractorFacade>();
+      InteractorFacade rightFacade = rightInteractor.gameObject.GetComponent<InteractorFacade>();
+      bool ungrabLeft = false;
+      for (int i = 0; i < leftFacade.GrabbedObjects.Count; i++) {
+        if (leftFacade.GrabbedObjects[i].gameObject.scene.name == "Tutorial")
+          ungrabLeft = true;
+      }
+      if (ungrabLeft)
+        rightFacade.Ungrab();
+
+      bool ungrabRight = false;
+      for (int i = 0; i < rightFacade.GrabbedObjects.Count; i++) {
+        if (rightFacade.GrabbedObjects[i].gameObject.scene.name == "Tutorial")
+          ungrabRight = true;
+      }
+      if (ungrabRight)
+        rightFacade.Ungrab();
+
       SceneManager.UnloadSceneAsync("Tutorial");
       menu.gameObject.SetActive(true);
       menu.gameObject.SendMessage("Enable");
