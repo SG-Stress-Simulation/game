f5b87055dc462076b6cb3ec5991ee72a0669ddf9
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2023-03-10 00:48:25 +0100

    adjusted vignette

--- Assets/Scripts/Effects/VignetteEffect.cs
+++ Assets/Scripts/Effects/VignetteEffect.cs
@@ -7,6 +7,7 @@ namespace Zinnia.Action.Effects
     public class VignetteEffect : BaseEffect
     {
         Vignette m_Vignette;
+        MotionBlur m_Blur;
         AudioSource[] m_AudioSources;
         private float timeToSecond = 0.0f;
         
@@ -14,6 +15,7 @@ namespace Zinnia.Action.Effects
         {
             base.StopEffect();
             m_Vignette.intensity.Override( 0f);
+            m_Blur.shutterAngle.Override(0f);
             foreach (var source in m_AudioSources)
             {
                 source.Stop();
@@ -36,7 +38,14 @@ namespace Zinnia.Action.Effects
             m_Vignette = ScriptableObject.CreateInstance<Vignette>();
             m_AudioSources = GetComponents<AudioSource>();
             m_Vignette.enabled.Override(true);
-            m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+            m_Vignette.color.Override(new Color(0.0f,0.0f,0.0f, 0.5f));
+
+            m_Blur = ScriptableObject.CreateInstance<MotionBlur>();
+            m_Blur.enabled.Override(true);
+            m_Blur.shutterAngle.Override(0.0f);
+            m_Blur.sampleCount.Override(16);
+            
+            m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette, m_Blur);
         }
 
         public override void Update()
@@ -52,6 +61,7 @@ namespace Zinnia.Action.Effects
                 float overAllIntensity = Mathf.Sin((timeToEffectEnd / duration) * Mathf.PI) * 2f * intensity;
                 float pulse = Mathf.Sin((timeToSecond / 0.75f) * Mathf.PI) * 0.1f;
                 m_Vignette.intensity.Override(overAllIntensity + pulse);
+                m_Blur.shutterAngle.Override(360f);
             }
             base.Update();
         }
